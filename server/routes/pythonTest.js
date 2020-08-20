68f011107553e87cfc6e183f783f6f45a562f979
Martin <office@martinpagels.de>
2020-08-21 00:42:41 +0200

    Add route for exec python script

--- server/routes/pythonTest.js
+++ server/routes/pythonTest.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import { PythonShell } from 'python-shell'
 
 const router = express.Router()
 
-router.get('/', (req, res) => res('Hello World'))
+router.get('/', (req, res) => {
+  console.log('request')
+  PythonShell.run(
+    '/Users/student/Documents/neuefische/podcaster/server/services/podcastScrapper.py',
+    null,
+    (err, data) => {
+      res.json(JSON.parse(data))
+    }
+  )
+})
 
 export default router
