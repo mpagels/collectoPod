ac95864018abdb7fe5dbbd33f767701c05909d23
Martin <office@martinpagels.de>
2020-08-24 01:44:21 +0200

    First design draft for PodCast

--- client/src/components/Podcast/Podcasts.js
+++ client/src/components/Podcast/Podcasts.js
@@ -17,28 +17,78 @@ export default function Podcasts({
   }
   return (
     <>
-      <Card key={key} onClick={() => handleClick()}>
-        <HeaderArea>
-          <EpisodeNumber>{nr}</EpisodeNumber>
-          <EpisodeTitle> {subtitle}</EpisodeTitle>
-        </HeaderArea>
+      <Card key={key} isOpen={isOpen}>
+        <Overview>
+          <EpisodeNr>{nr}</EpisodeNr>
+          <EpisodeInfos>
+            <EpisodeTitle>{subtitle}</EpisodeTitle>
+            <Published>Published: {publish}</Published>
+            <Duration>Duration: {duration}</Duration>
+          </EpisodeInfos>
+        </Overview>
         {isOpen && <Description>{description}</Description>}
+        <ReadMore onClick={() => handleClick()}>
+          {' '}
+          {isOpen ? 'CLOSE' : 'READ MORE...'}
+        </ReadMore>
+        <Download href={url}>DOWNLOAD</Download>
       </Card>
     </>
   )
 }
 
 const Card = styled.section`
-  background-color: grey;
+  display: grid;
+  grid-template-rows: 80px ${(props) => props.isOpen && 'auto'} 40px 60px;
+  background-color: #3c3c3e;
   border-radius: 10px;
+  margin: 15px;
+  box-shadow: 0 0 15px black;
 `
 
-const HeaderArea = styled.div`
+const Overview = styled.div`
+  display: grid;
+  grid-template-columns: 20% 80%;
+`
+
+const EpisodeNr = styled.div`
+  align-self: center;
+  justify-self: center;
+  font-size: 32px;
+`
+const EpisodeInfos = styled.div`
   display: flex;
+  flex-direction: column;
+`
+const EpisodeTitle = styled.div`
+  padding: 6px;
 `
 
-const EpisodeNumber = styled.div``
+const ReadMore = styled.div`
+  cursor: pointer;
+  text-align: center;
+  height: 100%;
+  align-self: center;
+  vertical-align: middle;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const Download = styled.a`
+  background-color: #50805b;
+  height: 100%;
+  border-radius: 0 0 10px 10px;
+  align-self: center;
+  vertical-align: middle;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`
+const Published = styled.div``
+const Duration = styled.div``
 
-const EpisodeTitle = styled.div``
-
-const Description = styled.div``
+const Description = styled.div`
+  text-align: justify;
+  padding: 25px;
+`
