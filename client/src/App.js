7cb11262fafbc0f3bbf1967bbc68b2253b0ae209
Martin <office@martinpagels.de>
2020-08-23 22:22:55 +0200

    First draft App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from 'react'
+import Podcast from './components/Podcast/Podcast'
+import StartPage from './pages/StartPage'
 function App() {
-  return (
-    <div>
-      <header>Hello World</header>
-    </div>
-  );
+  const [podcasts, setPodcasts] = useState([[]])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:4000')
+  //     .then((res) => res.json())
+  //     .then(setPodcasts)
+  // }, [])
+  // const mordLust = podcasts[0].map((podcast) => (
+  //   <Podcast
+  //     key={podcast.id}
+  //     nr={podcast.nr}
+  //     description={podcast.description}
+  //     subtitle={podcast.subtitle}
+  //   />
+  // ))
+
+  // return <>{mordLust}</>
+  return <StartPage />
 }
 
-export default App;
+export default App
