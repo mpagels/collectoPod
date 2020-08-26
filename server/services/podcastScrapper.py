88a99981df5c86f299e49d51d53870df8fd25975
Martin <office@martinpagels.de>
2020-08-27 00:55:30 +0200

    Change variable names & api res is now correct

--- server/services/podcastScrapper.py
+++ server/services/podcastScrapper.py
@@ -7,7 +7,7 @@ import sys
 
 def get_podcast_infos():
     
-    podcasts = [{ 
+    podcasts_to_search = [{ 
         "crime" : [
             {"verbrechen" : "https://rss.art19.com/verbrechen-von-nebenan-true-crime"},
         {"mordlust" : "https://rss.nexx.cloud/F0RCEXPNYW8MKX8"},
@@ -21,12 +21,12 @@ def get_podcast_infos():
         # {"eine_stunde_history": "http://www.deutschlandfunknova.de/podcast/eine-stunde-history"}]
     #}]
 
-    podcast_folgen = {
+    podcasts_to_send = {
         "crime" : [],
         "other" : []
     }
 
-    for podcast in podcasts:
+    for podcast in podcasts_to_search:
         for genre, list_of_obj_of_podcasts in podcast.items():
             for obj_of_podcast in list_of_obj_of_podcasts:
                 podCast = {}
@@ -41,7 +41,6 @@ def get_podcast_infos():
 
                     in_channel = root[0].getchildren()
                     all_items = []
-
                     for item in in_channel:
                         if item.tag == "item":
                             all_items.append(item)
@@ -54,7 +53,10 @@ def get_podcast_infos():
                         _id = str(len(podCast[podcast_name]))
                         folge["id"] = _id
 
-                        if podcast_name != "zeit_verbrechen" and podcast_name != "zeit_pfarrerstoechter":
+                        if (podcast_name != "zeit_verbrechen" 
+                        and podcast_name != "zeit_pfarrerstoechter" 
+                        and podcast_name != "darfs_ein_bisschen_mord_sein"
+                        and podcast_name != "verbrechen_der_vergangenheit"):
                             for node in item:
                                 if node.tag == "title":
                                     if node.text.startswith("#"):
@@ -70,14 +72,28 @@ def get_podcast_infos():
                                     folge["publish"] = node.text[:16]
                                 elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}duration":
                                     folge["duration"] = node.text
-                                
+
+                        elif podcast_name == "darfs_ein_bisschen_mord_sein" or podcast_name == "verbrechen_der_vergangenheit":
+                            for node in item:
+                                if node.tag == "title":
+                                    folge["subtitle"] = node.text
+                                elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode":
+                                    folge["nr"] = "#" + node.text
+                                elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}summary": #"{http://purl.org/rss/1.0/modules/content/}encoded":
+                                    folge["description"] = node.text
+                                elif node.tag == "enclosure":
+                                    folge["url"] = node.attrib["url"]
+                                elif node.tag == "pubDate":
+                                    folge["publish"] = node.text[:16]
+                                elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}duration":
+                                    folge["duration"] = node.text        
                         else:
                             for node in item:
                                 if node.tag == "title":
                                     folge["subtitle"] = node.text
                                 elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}episode":
                                     folge["nr"] = "#" + node.text
-                                elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}summary":
+                                elif node.tag == "{http://www.itunes.com/dtds/podcast-1.0.dtd}summary": #"{http://purl.org/rss/1.0/modules/content/}encoded":
                                     folge["description"] = node.text
                                 elif node.tag == "enclosure":
                                     folge["url"] = node.attrib["url"]
@@ -87,9 +103,9 @@ def get_podcast_infos():
                                     folge["duration"] = "0" + str(datetime.timedelta(seconds=int(node.text)))
                         
                         podCast[podcast_name].append(folge)
-                    podcast_folgen[genre].append(podCast)
+                    podcasts_to_send[genre].append(podCast)
 
-    print(json.dumps([podcast_folgen], ensure_ascii=False).encode('utf8').decode())
+    print(json.dumps([podcasts_to_send], ensure_ascii=False).encode('utf8').decode())
     sys.stdout.flush()
 
 get_podcast_infos()
