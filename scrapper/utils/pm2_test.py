35d4b9babd1c900bf3bfb1c47cef385bdc14693f
Martin Pagels <office@martinpagels.de>
2020-12-09 20:21:20 +0100

    Add logger to import statement

--- scrapper/utils/pm2_test.py
+++ scrapper/utils/pm2_test.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import logging
+try:
+    import telegram
+except Exception as e:
+    logging.error(e)
 
 logging.basicConfig(filename='pm2_logger.log',level=logging.INFO, format="%(levelname)s:%(asctime)s:%(message)s")
 
